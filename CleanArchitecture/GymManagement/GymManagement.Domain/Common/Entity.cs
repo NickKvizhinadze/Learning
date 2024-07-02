b113699ca4ebc8739d061f713b02e479c3724df7
Nick Kvizhinadze <nickkvizhinadze@hotmail.com>
2024-07-02 19:45:03 +0400

    CleanArchitecture => Added domain events

---
+++ CleanArchitecture/GymManagement/GymManagement.Domain/Common/Entity.cs
@@ -0,0 +1,17 @@
+﻿namespace GymManagement.Domain.Common;
+
+public abstract class Entity
+{
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    protected Entity()
+    {
+    }
+    
+    public Guid Id { get; set; }
+
+    protected readonly List<IDomainEvent> _domainEvents = [];
+}
